eed63b3e165d8d89fcd868e7269dc42cb661458c
zhuozhi12 <feibou@126.com>
2017-04-23 09:11:18 +0800

    niklin viedo share

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module JobListing
   class Application < Rails::Application
+    config.assets.paths << "#{Rails.root}/app/assets/videos"  #// landing page 背景视频
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
